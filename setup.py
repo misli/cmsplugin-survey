e850b23ba3a1fa442b28c2009733c774c5d3f892
Jakub Dorňák <jdornak@redhat.com>
2016-12-03 19:43:06 +0100

    addon.json, MANIFEST, setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
-
 from setuptools import setup, find_packages
+
 
 setup(
     name            = 'cmsplugin-survey',
