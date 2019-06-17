a739b017ba7e5d35149cc3b8af0b973703310e23
Jakub Dorňák <jakub.dornak@misli.cz>
2019-06-17 11:20:03 +0200

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name            = 'cmsplugin-survey',
-    version         = '1.2.0',
+    version         = '1.2.1',
     description     = 'django CMS plugin to show simple surveys',
     author          = 'Jakub Dorňák',
     author_email    = 'jakub.dornak@misli.cz',
