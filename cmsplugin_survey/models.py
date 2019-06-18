84096949cd0e2a0ff3886806b377ef6ee6c4ec1b
Jakub Dorňák <jakub.dornak@misli.cz>
2019-06-18 08:34:14 +0200

    fix broken import

--- cmsplugin_survey/models.py
+++ cmsplugin_survey/models.py
@@ -2,12 +2,12 @@ from __future__ import unicode_literals
 
 from cms.models import CMSPlugin
 from django import forms
+from django.conf import settings
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
 
-from .conf import settings
 from .fields import ColorField
 
 
