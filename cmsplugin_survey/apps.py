5408d5e341b6546a05d62e6651b57518136d9864
Jakub Dorňák <jakub.dornak@misli.cz>
2019-06-19 14:40:10 +0200

    fix app_conf

--- cmsplugin_survey/apps.py
+++ cmsplugin_survey/apps.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from appconf import AppConf
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -7,3 +8,12 @@ from django.utils.translation import ugettext_lazy as _
 class CmspluginSurveyConfig(AppConfig):
     name = 'cmsplugin_survey'
     verbose_name = _('django CMS Surveys')
+
+
+class CmspluginSurveyAppConf(AppConf):
+    TEMPLATES = [
+        ('default', _('default')),
+    ]
+
+    class Meta:
+        prefix = 'CMSPLUGIN_SURVEY'
